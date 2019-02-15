a331c25ae3e29f89312548fa93cb37c03e8fbf00
Tobias Faßbender <tob.fassbender@fz-juelich.de>
2019-02-15 10:04:48 +0100

    changed the test class PhyphoxDataTest to PhyphoxExperimentTest because of the chaned name of the class under test; also added some test cases

--- src/test/java/de/fz_juelich/phyphox_interface/data/PhyphoxExperimentTest.java
+++ src/test/java/de/fz_juelich/phyphox_interface/data/PhyphoxExperimentTest.java
@@ -3,52 +3,57 @@ package de.fz_juelich.phyphox_interface.data;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Matchers;
 
 import de.fz_juelich.phyphox_interface.connection.PhyphoxConnection;
 import de.fz_juelich.phyphox_interface.connection.PhyphoxDataRequest;
 
-class PhyphoxDataTest {
+class PhyphoxExperimentTest {
 	
 	private static final double epsilon = 1e-5;
 	
 	@Test
 	public void testContinuesBuffer() {
-		PhyphoxExperiment data = new PhyphoxExperiment("time", "buffer_x");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
 		
-		assertEquals(2, data.getAllData().size());
+		assertEquals(2, experiment.getAllData().size());
 		//assertEquals(1, data.getContinuesBufferIndex());
 	}
 	
 	@Test
 	public void testGetBuffers() {
-		PhyphoxExperiment data = new PhyphoxExperiment("time", "buffer_x");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
 		
 		PhyphoxBuffer buffer1 = new PhyphoxBuffer("time", new double[] {1, 2, 3});
 		List<PhyphoxBuffer> buffers = new ArrayList<PhyphoxBuffer>(2);
 		buffers.add(buffer1);
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
-		PhyphoxBuffer bufferTime = data.getBufferData("time");
+		PhyphoxBuffer bufferTime = experiment.getBufferData("time");
 		assertEquals(3, bufferTime.size());
 		assertArrayEquals(new double[] {1, 2, 3}, bufferTime.getData(), epsilon);
 	}
 	
 	@Test
 	public void testGetBuffers_wrongBufferName() {
-		PhyphoxExperiment data = new PhyphoxExperiment("time", "buffer_x");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
 		
-		assertThrows(IllegalStateException.class, () -> data.getBufferData("non_existing_buffer_name"));
+		assertThrows(IllegalStateException.class, () -> experiment.getBufferData("non_existing_buffer_name"));
 	}
 	
 	@Test
 	public void testAddNewDataToBuffers() {
-		PhyphoxExperiment data = new PhyphoxExperiment("time", "buffer_x", "time");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x", "time");
 		
 		PhyphoxBuffer buffer1 = new PhyphoxBuffer("time", new double[] {1, 2, 3});
 		PhyphoxBuffer buffer2 = new PhyphoxBuffer("buffer_x", new double[] {42, 43, 44, 45});
@@ -57,10 +62,10 @@ class PhyphoxDataTest {
 		buffers.add(buffer1);
 		buffers.add(buffer2);
 		
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
-		PhyphoxBuffer bufferX = data.getBufferData("buffer_x");
-		PhyphoxBuffer bufferTime = data.getBufferData("time");
+		PhyphoxBuffer bufferX = experiment.getBufferData("buffer_x");
+		PhyphoxBuffer bufferTime = experiment.getBufferData("time");
 		assertEquals(4, bufferX.size());
 		assertArrayEquals(new double[] {42, 43, 44, 45}, bufferX.getData(), epsilon);
 		assertEquals(3, bufferTime.size());
@@ -69,29 +74,29 @@ class PhyphoxDataTest {
 	
 	@Test
 	public void testAddNewDataToBuffers_noContinuesBuffer() {
-		PhyphoxExperiment data = new PhyphoxExperiment("buffer_x");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("buffer_x");
 		
 		PhyphoxBuffer buffer = new PhyphoxBuffer("buffer_x", new double[] {42, 43, 44, 45});
 		List<PhyphoxBuffer> buffers = new ArrayList<PhyphoxBuffer>(2);
 		buffers.add(buffer);
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
 		//add some more data; the old data appended in this version of the full buffer implementation
 		buffer = new PhyphoxBuffer("buffer_x", new double[] {1, 2, 3});
 		buffers.clear();
 		buffers.add(buffer);
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
-		PhyphoxBuffer bufferX = data.getBufferData("buffer_x");
+		PhyphoxBuffer bufferX = experiment.getBufferData("buffer_x");
 		assertEquals(7, bufferX.size());//the data is added and not overwritten
 		assertArrayEquals(new double[] {42, 43, 44, 45, 1, 2, 3}, bufferX.getData(), epsilon);
 	}
 	
 	@Test
 	public void testCreateRequest() {
-		PhyphoxExperiment data = new PhyphoxExperiment("time", "buffer_x");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
 		
-		PhyphoxDataRequest request = data.createRequestForNewData();
+		PhyphoxDataRequest request = experiment.createRequestForNewData();
 		String requestText = request.getAsString();
 		
 		//only full updates are used in this implementation
@@ -100,9 +105,9 @@ class PhyphoxDataTest {
 	
 	@Test
 	public void testCreateRequest_noContinuesBuffer() {
-		PhyphoxExperiment data = new PhyphoxExperiment("buffer_x", "buffer_y");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("buffer_x", "buffer_y");
 		
-		PhyphoxDataRequest request = data.createRequestForNewData();
+		PhyphoxDataRequest request = experiment.createRequestForNewData();
 		String requestText = request.getAsString();
 		
 		assertEquals("buffer_x=full&buffer_y=full", requestText);
@@ -110,22 +115,22 @@ class PhyphoxDataTest {
 	
 	@Test
 	public void testGetNewData() {
-		PhyphoxExperiment data = new PhyphoxExperiment("time", "buffer_x");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
 		
 		PhyphoxBuffer buffer = new PhyphoxBuffer("buffer_x", new double[] {42, 43, 44, 45});
 		List<PhyphoxBuffer> buffers = new ArrayList<PhyphoxBuffer>(2);
 		buffers.add(buffer);
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
-		PhyphoxBuffer completeData = data.getBufferData("buffer_x");
+		PhyphoxBuffer completeData = experiment.getBufferData("buffer_x");
 		
 		//add some new data
 		buffer = new PhyphoxBuffer("buffer_x", new double[] {1, 2, 3});
 		buffers.clear();
 		buffers.add(buffer);
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
-		PhyphoxBuffer bufferNewData = data.getNewBufferData("buffer_x");
+		PhyphoxBuffer bufferNewData = experiment.getNewBufferData("buffer_x");
 		assertEquals(3, bufferNewData.size());
 		assertArrayEquals(new double[] {1, 2, 3}, bufferNewData.getData(), epsilon);
 		assertEquals(7, completeData.size());
@@ -134,23 +139,23 @@ class PhyphoxDataTest {
 	
 	@Test
 	public void testClearBuffers() {
-		PhyphoxExperiment data = new PhyphoxExperiment("time", "buffer_x");
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
 		
 		PhyphoxBuffer buffer = new PhyphoxBuffer("buffer_x", new double[] {42, 43, 44, 45});
 		List<PhyphoxBuffer> buffers = new ArrayList<PhyphoxBuffer>(2);
 		buffers.add(buffer);
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
 		//clear all buffers
-		data.clearAllBuffers();
+		experiment.clearAllBuffers();
 		
 		//add some new data
 		buffer = new PhyphoxBuffer("buffer_x", new double[] {1, 2, 3});
 		buffers.clear();
 		buffers.add(buffer);
-		data.addNewDataToBuffers(buffers);
+		experiment.addNewDataToBuffers(buffers);
 		
-		PhyphoxBuffer bufferNewData = data.getNewBufferData("buffer_x");
+		PhyphoxBuffer bufferNewData = experiment.getNewBufferData("buffer_x");
 		assertEquals(3, bufferNewData.size());
 		assertArrayEquals(new double[] {1, 2, 3}, bufferNewData.getData(), epsilon);
 	}
@@ -161,5 +166,38 @@ class PhyphoxDataTest {
 		//the object has to be constructed correctly but the update thread will cause a runtime exception
 		List<String> bufferNames = Arrays.asList(new String[] {"buffer_x", "buffer_y"});
 		new PhyphoxExperiment(new PhyphoxConnection("1.1.1.1", 42), bufferNames, 1000);
+	}
+	
+	@Test
+	public void testListenerPattern() {
+		PhyphoxDataListener listener = mock(PhyphoxDataListener.class);
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
+		experiment.addDataListener(listener);
+		
+		PhyphoxBuffer buffer = new PhyphoxBuffer("buffer_x", new double[] {42, 43, 44, 45});
+		List<PhyphoxBuffer> buffers = new ArrayList<PhyphoxBuffer>(2);
+		buffers.add(buffer);
+		experiment.addNewDataToBuffers(buffers);
+		
+		verify(listener, times(1)).updateData(Matchers.anyListOf(PhyphoxBuffer.class), any(Boolean.class));
+	}
+	
+	@Test
+	public void testAddAndRemoveListeners() {
+		PhyphoxDataListener listener = mock(PhyphoxDataListener.class);
+		PhyphoxExperiment experiment = new PhyphoxExperiment("time", "buffer_x");
+		experiment.addDataListener(listener);
+		
+		PhyphoxBuffer buffer = new PhyphoxBuffer("buffer_x", new double[] {1, 2, 3});
+		List<PhyphoxBuffer> buffers = new ArrayList<PhyphoxBuffer>(Arrays.asList(new PhyphoxBuffer[] {buffer}));
+		experiment.addNewDataToBuffers(buffers);
+		
+		//the update method was called
+		verify(listener, times(1)).updateData(Matchers.anyListOf(PhyphoxBuffer.class), any(Boolean.class));
+		
+		experiment.removeDataListener(listener);
+		
+		//the update method is still called only once
+		verify(listener, times(1)).updateData(Matchers.anyListOf(PhyphoxBuffer.class), any(Boolean.class));
 	}
 }
